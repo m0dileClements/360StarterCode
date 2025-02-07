cdbba8351029223c5044081b4cafd499582ec2af
m0dileClements <molly.clements@centre.edu>
2025-02-07 08:40:54 -0500

    Person Eclipse Challenge Files

---
+++ src/main/java/UnderAgeException.java
@@ -0,0 +1,29 @@
+
+
+public class UnderAgeException extends Exception
+{
+	private static final long serialVersionUID = 4554479932935169519L;
+
+	/**
+	 * 
+	 */
+	public UnderAgeException()
+	{
+		super("Parent is too young! Noooo!");
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public UnderAgeException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,64 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonTest
+{
+
+	Person R;
+	Person J;
+	Person M;
+	Person K;
+
+	
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		R = new Person("Romeo", "Mon", 15);
+		J = new Person("Juliette", "Cap", 13);
+		M = new Person("Count", "Dooku", 34);
+		K = new Person("General", "Kenobi", 30);
+	}
+
+	@Test
+	void testToString()
+	{
+		assertEquals("Person: Romeo Mon, 15", R.toString());
+//		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testPerson()
+	{
+		assertThrows(UnderAgeException.class, () -> {
+			R.makePerson(J);
+			
+		});
+		
+		assertThrows(UnderAgeException.class, () -> {
+			M.makePerson(J);
+			
+		});
+		
+		
+		
+		Person child;
+		try
+		{
+			child = M.makePerson(K);
+			assertEquals(M.getFname(), child.getFname());
+			assertEquals(K.getLname(), child.getLname());
+			assertEquals(0, child.getAge());
+			
+		} catch (UnderAgeException e)
+		{
+			e.printStackTrace();
+			fail("Nope. Exception station. Toot! Toot!");
+		}
+		
+	}
+
+}
